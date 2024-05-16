e9291f562dd6e7725660680321658de457196073
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-05-16 15:26:46 -0400

    Changement du design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,8 @@ import { ABOUT_TEXT } from '../constants/index';
 const About = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-      <h2 className='my-20 text-center text-4xl'>About
-      <span className='text-neutral-500'> Me</span> </h2>
+      <h2 className='my-20 text-center text-4xl'>À  
+      <span className='text-neutral-500'> propos</span> </h2>
       <div>
         <p className='my-2 maxw-xl text-center text-xl'>{ABOUT_TEXT}</p>
       </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,7 +8,7 @@ import { SiAngular } from "react-icons/si";
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-4xl">Technologies</h2>
+      <h2 className="my-20 text-4xl text-center">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <RiReactjsLine className="text-7xl" style={{ color: "#61DBFB" }} />
