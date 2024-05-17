b8f7d2d3a08abed8ef89c34388e7bfb1ff84eef2
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-05-17 13:13:27 -0400

    Ajout des liens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,21 @@ import logo from "../assets/Logowinpng.png";
 
 const Navbar = () => {
   return (
-    <nav className=" mb-10 flex items-center justify-between py-4">
+    <nav className="mb-10 flex items-center justify-between py-4">
       <div className="flex flex-shrink-0 items-center ml-4">
-        <img src={logo} alt="log" className="w-24 h-24"/>
+        <img src={logo} alt="log" className="w-24 h-24" />
       </div>
 
       <div className="mr-4 flex items-center justify-center gap-4 text-xl">
-        <AiFillLinkedin />
-        <AiFillGithub />
-        <AiFillInstagram />
+        <a href="https://www.linkedin.com/in/winner-pindi-768400235/" target="_blank" rel="noopener noreferrer">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/WinnerPindi" target="_blank" rel="noopener noreferrer">
+          <AiFillGithub />
+        </a>
+        <a href="https://www.instagram.com/winner_p78/" target="_blank" rel="noopener noreferrer">
+          <AiFillInstagram />
+        </a>
       </div>
     </nav>
   );
